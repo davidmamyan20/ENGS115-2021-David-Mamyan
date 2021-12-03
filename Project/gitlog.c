8e445fe07c62ff1ec66278d5415e614725fb4d6a
davidmamyan20 <david_mamyan@edu.aua.am>
2021-12-03 09:48:40 -0600

    Linux:

---
+++ Project/gitlog.c
@@ -0,0 +1,25 @@
+#include stdio.h
+#include stdlib.h
+
+void print_gitlog()
+{
+	//TODO
+}
+void gitlog_json()
+{
+	//TODO
+}
+void convert_to_html()
+{
+	//TODO
+}
+void filter()
+{
+	//Optional
+}
+void main()
+{
+	//TODO
+}
+
+
